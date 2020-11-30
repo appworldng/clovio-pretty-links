a71beeef5b5697553c8d495297a206b4dcccd134
Chigozie Orunta <chigozieorunta@yahoo.com>
2020-11-30 13:08:23 +0100

    Added main plugin file

---
+++ clovio-pretty-link.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Clovio Pretty Link
+ * Plugin URI:      github.com/chigozieorunta/clovio-pretty-link
+ * Description:     Display list of Pretty links and prevent user with Author roles from gaining access to main Pretty links.
+ * Author:          Chigozie Orunt
+ * Author URI:      github.com/chigozieorunta
+ * Text Domain:     clovio-pretty-link
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Clovio_Pretty_Link
+ */
+
+// Your code starts here.
